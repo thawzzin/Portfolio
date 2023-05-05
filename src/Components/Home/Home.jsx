d98ffdb893476d34741983e15ad11fdaaa2524ad
Thaw Zin <thawzzin.16@gmail.com>
2023-05-05 17:22:54 +0630

    animation upgrade

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,9 +8,35 @@ const Home = () => {
       <Header />
       <main className="flex items-center h-full">
         <article className="md:w-1/2">
-          <h5 className="uppercase font-medium">Hi, I'm Thaw Zin</h5>
-          <h1 className="uppercase font-bold text-5xl md:text-6xl mb-3">
-            I'm a frontend developer
+          <h5 className="uppercase font-medium ">Hi, I'm Thaw Zin</h5>
+          <h1 className="uppercase font-bold text-5xl md:text-6xl mb-3 waviy">
+            {/* I'm a frontend developer */}
+            <span style={{ "--i": 1 }}>I'm</span>
+            &nbsp;
+            <span style={{ "--i": 2 }}>a</span>
+            &nbsp;
+            <div className="inline-block">
+
+            <span style={{ "--i": 4 }}>f</span>
+            <span style={{ "--i": 5 }}>r</span>
+            <span style={{ "--i": 6 }}>o</span>
+            <span style={{ "--i": 7 }}>n</span>
+            <span style={{ "--i": 8 }}>t</span>
+            <span style={{ "--i": 9 }}>e</span>
+            <span style={{ "--i": 10 }}>n</span>
+            <span style={{ "--i": 11 }}>d</span>
+            </div>
+            &nbsp;
+            <br />
+            <span style={{ "--i": 12 }}>d</span>
+            <span style={{ "--i": 13 }}>e</span>
+            <span style={{ "--i": 14 }}>v</span>
+            <span style={{ "--i": 15 }}>e</span>
+            <span style={{ "--i": 16 }}>l</span>
+            <span style={{ "--i": 17 }}>o</span>
+            <span style={{ "--i": 18 }}>p</span>
+            <span style={{ "--i": 19 }}>e</span>
+            <span style={{ "--i": 20 }}>r</span>
           </h1>
           <p>
             A frontend developer who write clean and efficient code and
